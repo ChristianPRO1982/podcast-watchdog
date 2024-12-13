1b1b424f4a9cbab29b8f029ef0fb25b84f5b9874
ChristianPRO1982 <christian.martiny@outlook.com>
2024-12-13 14:30:47 +0100

    .

---
+++ src/main.py
@@ -0,0 +1,16 @@
+from src.logs import init_log, logging_msg
+
+
+
+### DOWNLOAD PODCASTS ###
+def parse_rss_feed():
+    pass
+
+
+############
+### MAIN ###
+############
+init_log()
+logging_msg("START PROGRAM", "WARNING")
+test()
+logging_msg("END PROGRAM", "WARNING")
