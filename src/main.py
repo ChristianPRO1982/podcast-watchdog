0c8d02dc6ef08eb065f71baa6fcbe5d2bf6fb536
ChristianPRO1982 <christian.martiny@outlook.com>
2024-12-13 15:42:39 +0100

    .

--- src/main.py
+++ src/main.py
@@ -1,10 +1,21 @@
+import os
+import dotenv
+import json
 from src.logs import init_log, logging_msg
 
 
 
+dotenv.load_dotenv('.env', override=True)
+DEBUG = os.getenv("DEBUG")
+RSS_FEEDS = os.getenv("RSS_FEEDS")
+
+
 ### DOWNLOAD PODCASTS ###
 def parse_rss_feed():
-    pass
+    print(os.path.abspath(RSS_FEEDS))
+    rss_feeds = json.loads(os.path.abspath(RSS_FEEDS))
+    # for feed in rss_feeds:
+    #     print(f"Category: {feed['category']}, Name: {feed['name']}, RSS Feed: {feed['rss_feed']}")
 
 
 ############
@@ -12,5 +23,5 @@ def parse_rss_feed():
 ############
 init_log()
 logging_msg("START PROGRAM", "WARNING")
-test()
+parse_rss_feed()
 logging_msg("END PROGRAM", "WARNING")
