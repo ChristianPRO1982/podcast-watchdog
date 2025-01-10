09dbd7ecbdb0d22b125b1b48a78a818b366a2537
ChristianPRO1982 <christian.martiny@outlook.com>
2025-01-10 10:23:53 +0100

    sdf

--- test/test_prog.py
+++ test/test_prog.py
@@ -1,5 +1,5 @@
 import pytest
-from src.prog import add, multiply
+from src.prog import add, multiply, subtract
 
 def test_add():
     assert add(2, 3) == 5
@@ -10,4 +10,8 @@ def test_multiply():
     assert multiply(2, 3) == 6
     assert multiply(-1, 1) == -1
     assert multiply(0, 0) == 0
-    
+
+def test_subtract():
+    assert subtract(2, 3) == -1
+    assert subtract(-1, 1) == -2
+    assert subtract(0, 0) == 0
