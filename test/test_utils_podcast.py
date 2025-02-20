e9209c61c102c361118a1d6d37eeffc03636a293
Christian Martiny <christian.martiny@outlook.com>
2025-02-20 10:13:10 +0100

    feat: pytest transcribe

--- test/test_utils_podcast.py
+++ test/test_utils_podcast.py
@@ -1,10 +1,17 @@
 import pytest
 import dotenv
 import os
+import shutil
 from src.logs import Logs
 from src.utils_sqlite import PodcastDB
 from src.utils_podcast import Podcasts
 
+
+if os.path.exists('./downloads/example.mp3'):
+    os.remove('./downloads/example.mp3')
+if os.path.exists('./downloads/example.txt'):
+    os.remove('./downloads/example.txt')
+shutil.copy('./test/example.mp3', './downloads')
 
 dotenv.load_dotenv(override=True)
 DEBUG = os.getenv("DEBUG")
@@ -19,6 +26,25 @@ def test_podcasts():
         count_before = podcastdb.count_podcasts(downloaded=False)
         podcasts.download_podcasts()
         count_after = podcastdb.count_podcasts(downloaded=False)
+        assert count_before - 1 == count_after
+    
+    else:
+        assert False
+
+
+def test_transcribe():
+    if DEBUG == '4':
+        podcastdb.insert_podcast('category', 'test_transcribe', 'rss_feed', 'title', 'test_transcribe', 'published', 'description')
+        request = '''
+UPDATE podcasts
+   SET downloaded = 1
+ WHERE podcast_name = "test_transcribe"
+'''
+        podcastdb.update_podcast(request)
+
+        count_before = podcastdb.count_podcasts(downloaded=True, processed=False)
+        podcasts.transcribe_podcasts()
+        count_after = podcastdb.count_podcasts(downloaded=True, processed=False)
         assert count_before - 1 == count_after
     
     else:
